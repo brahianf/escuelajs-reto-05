07117ac7588e66560b7e7904d5a9e98477479d2e
brahianf <brahianf@unicauca.edu.co>
2020-07-25 21:18:55 -0500

    segundo problema

--- src/index.js
+++ src/index.js
@@ -2,30 +2,38 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      localStorage.setItem('next_fetch',response.info.next)
-      const characters = response.results;
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+const getData = async (api) => {
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    if(data){
+      localStorage.setItem('next_fetch', JSON.stringify(data.info.next))
+      return data;
+    }
+  }catch (error){
+    console.log(error);
+  }
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async () => {
+  var next_fetch=localStorage.getItem('next_fetch')
+  if(next_fetch===null){
+    const data= await getData(API);
+    localStorage.setItem('characters',JSON.stringify(data.results));
+  }
+  const characters = (JSON.parse(localStorage.getItem('characters')))
+  let output = characters.map(character => {
+  return `
+  <article class="Card">
+    <img src="${character.image}" />
+    <h2>${character.name}<span>${character.species}</span></h2>
+  </article>
+  `
+  }).join('');
+  let newItem = document.createElement('section');
+  newItem.classList.add('Items');
+  newItem.innerHTML = output;
+  $app.appendChild(newItem);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
